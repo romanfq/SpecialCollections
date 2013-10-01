3bb062308210e016343516df78e7569432423649
Roman Fresneda-Quiroga <roman.fq@gmail.com>
2013-10-02 00:47:42 +0100

    Removed tail from linked list in ConcurrentCache, not needed

--- src/ConcurrentLRUCache.cs
+++ src/ConcurrentLRUCache.cs
@@ -71,179 +71,169 @@ namespace SpecialCollections
                 list.Use(value);
             }
         }
-    }
 
-    internal class LRUList<T> where T : class, IEquatable<T>
-    {
-        private readonly int _maxSize;
-        private int _size;
-
-        private readonly LRUEntry _head;
-        private readonly LRUEntry _tail;
-
-        private readonly ReaderWriterLockSlim _syncLock = new ReaderWriterLockSlim();
-
-        public LRUList(int maxSize)
+        class LRUList<T> where T : class, IEquatable<T>
         {
-            _maxSize = maxSize;
-            _head = new LRUEntry("h")
-                {
-                    Next = _tail = new LRUEntry("t")
-                };
-        }
+            private readonly int _maxSize;
+            private int _size;
 
-        public int MaxSize
-        {
-            get { return _maxSize; }
-        }
+            private readonly LRUEntry _head;
+            private readonly ReaderWriterLockSlim _syncLock = new ReaderWriterLockSlim();
 
-        public int Size
-        {
-            get { return _size; }
-        }
-
-        public void Add(T value)
-        {
-            if (value == null)
-                throw new ArgumentNullException("value");
-
-            try
+            public LRUList(int maxSize)
             {
-                // invariant: no more than _maxSize elements
-                _syncLock.EnterWriteLock();
+                _maxSize = maxSize;
+                _head = new LRUEntry("h");
+            }
 
-                // seek the value, bail out if found
+            public int Size
+            {
+                get { return _size; }
+            }
+
+            public void Add(T value)
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+
+                try
                 {
+                    // invariant: no more than _maxSize elements
+                    _syncLock.EnterWriteLock();
+
+                    // seek the value, bail out if found
+                    {
+                        var cursor = _head;
+                        while (cursor != null)
+                        {
+                            if (value.Equals(cursor.Value))
+                            {
+                                return;
+                            }
+                            cursor = cursor.Next;
+                        }
+                    }
+
+                    // insert the value, keep invariant _size <= maxSize
+                    {
+                        var newEntry = new LRUEntry(value);
+                        if (_size == _maxSize)
+                        {
+                            var cursor = _head;
+                            var prev = cursor;
+                            while (cursor.Next != null)
+                            {
+                                prev = cursor;
+                                cursor = cursor.Next;
+                            }
+
+                            cursor.Next = null;
+                            prev.Next = null;
+                        }
+                        else
+                        {
+                            _size++;
+                        }
+                        // h -> a1 -> a2 -> ... -> a(N-1) -> aN = tA
+                        newEntry.Next = _head.Next;
+                        _head.Next = newEntry;
+                    }
+                }
+                finally
+                {
+                    _syncLock.ExitWriteLock();
+                }
+            }
+
+            public void Use(T value)
+            {
+                try
+                {
+                    _syncLock.EnterWriteLock();
+
+                    // seek the value, bail out if not found
                     var cursor = _head;
-                    while (cursor != _tail)
+                    var prev = _head;
+                    while (cursor != null)
                     {
                         if (value.Equals(cursor.Value))
                         {
-                            return;
+                            break;
                         }
+
+                        prev = cursor;
                         cursor = cursor.Next;
                     }
-                }
 
-                // insert the value, keep invariant _size <= maxSize
-                {
-                    var newEntry = new LRUEntry(value);
-                    if (_size == _maxSize)
+                    if (cursor == null)
                     {
-                        var cursor = _head;
-                        var prev = cursor;
-                        while (cursor.Next != _tail)
-                        {
-                            prev = cursor;
-                            cursor = cursor.Next;
-                        }
-
-                        cursor.Next = null;
-                        prev.Next = _tail;
-                    }
-                    else
-                    {
-                        _size++;
-                    }
-                    // h -> a1 -> a2 -> ... -> a(N-1) -> aN = tA
-                    newEntry.Next = _head.Next;
-                    _head.Next = newEntry;
-                }
-            }
-            finally
-            {
-                _syncLock.ExitWriteLock();
-            }
-        }
-
-        public void Use(T value)
-        {
-            try
-            {
-                _syncLock.EnterWriteLock();
-
-                // seek the value, bail out if not found
-                var cursor = _head;
-                var prev = _head;
-                while (cursor != null)
-                {
-                    if (value.Equals(cursor.Value))
-                    {
-                        break;
+                        throw new ArgumentException("Specified value is not present in the list", "value");
                     }
 
-                    prev = cursor;
-                    cursor = cursor.Next;
-                }
+                    // adjust the entries around the cursor
+                    var nextByUsage = cursor.Next;
+                    prev.Next = nextByUsage;
 
-                if (cursor == null)
+                    // move the cursor to the start of the list
+                    if (cursor != _head.Next)
+                    {
+                        var moreRecentlyUsed = _head.Next;
+                        _head.Next = cursor;
+                        cursor.Next = moreRecentlyUsed;
+                    }
+                }
+                finally
                 {
-                    throw new ArgumentException("Specified value is not present in the list", "value");
+                    _syncLock.ExitWriteLock();
                 }
+            }
 
-                // adjust the entries around the cursor
-                var nextByUsage = cursor.Next;
-                prev.Next = nextByUsage;
-
-                // move the cursor to the start of the list
-                if (cursor != _head.Next)
+            public void CopyTo(T[] array, int index)
+            {
+                try
                 {
-                    var moreRecentlyUsed = _head.Next;
-                    _head.Next = cursor;
-                    cursor.Next = moreRecentlyUsed;
+                    _syncLock.EnterWriteLock();
+
+                    var cursor = _head.Next;
+                    int pos = 0;
+                    while (cursor != null)
+                    {
+                        array[index + pos] = cursor.Value;
+                        cursor = cursor.Next;
+                        pos++;
+                    }
                 }
-            }
-            finally
-            {
-                _syncLock.ExitWriteLock();
-            }
-        }
-
-        public void CopyTo(T[] array, int index)
-        {
-            try
-            {
-                _syncLock.EnterWriteLock();
-
-                var cursor = _head.Next;
-                int pos = 0;
-                while (cursor != _tail)
+                finally
                 {
-                    array[index + pos] = cursor.Value;
-                    cursor = cursor.Next;
-                    pos++;
+                    _syncLock.ExitWriteLock();
                 }
             }
-            finally
+
+            class LRUEntry
             {
-                _syncLock.ExitWriteLock();
-            }
-        }
+                private readonly string _name;
+                public T Value { get; private set; }
 
-        class LRUEntry
-        {
-            private readonly string _name;
-            public T Value { get; private set; }
+                public LRUEntry(T value)
+                {
+                    Value = value;
+                }
 
-            public LRUEntry(T value)
-            {
-                Value = value;
-            }
+                public LRUEntry(string name)
+                {
+                    _name = name;
+                }
 
-            public LRUEntry(string name)
-            {
-                _name = name;
-            }
+                public LRUEntry Next { get; set; }
 
-            public LRUEntry Next { get; set; }
-
-            public override string ToString()
-            {
-                return !string.IsNullOrWhiteSpace(_name)
-                        ? _name
-                        : Value == null
-                            ? "<NULL>"
-                            : Value.ToString();
+                public override string ToString()
+                {
+                    return !string.IsNullOrWhiteSpace(_name)
+                            ? _name
+                            : Value == null
+                                ? "<NULL>"
+                                : Value.ToString();
+                }
             }
         }
     }
